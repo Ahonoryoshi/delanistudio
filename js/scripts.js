b62145de75e3014535f74df38335e6d978b6d31b
Ahonoryoshi <yoshiahona@gmail.com>
2022-03-15 17:50:42 +0300

    add toggle effect on the 'what we do' icons

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $('#des').click(function(){
+        $('#des').toggle()
+        $('.des').toggle()
+    })
+    $('.des').click(function(){
+        $('#des').toggle()
+        $('.des').toggle()
+    })
+
+
+    $('#dev').click(function(){
+        $('#dev').toggle()
+        $('.dev').toggle()
+    })
+    $('.dev').click(function(){
+        $('#dev').toggle()
+        $('.dev').toggle()
+    })
+
+
+    $('#pro').click(function(){
+        $('#pro').toggle()
+        $('.pro').toggle()
+    })
+    $('.pro').click(function(){
+        $('#pro').toggle()
+        $('.pro').toggle()
+    })
+
+});
