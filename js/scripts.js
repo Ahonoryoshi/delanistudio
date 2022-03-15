f1ddfedbc1b8d0b88b82adb1c82f79deb971fa54
Ahonoryoshi <yoshiahona@gmail.com>
2022-03-15 17:53:51 +0300

    add code to alert user on submitting message

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,39 @@
 $(document).ready(function(){
     $('#des').click(function(){
-        $('#des').toggle()
-        $('.des').toggle()
-    })
+        $('#des').toggle();
+        $('.des').toggle();
+    });
     $('.des').click(function(){
-        $('#des').toggle()
-        $('.des').toggle()
-    })
+        $('#des').toggle();
+        $('.des').toggle();
+    });
 
 
     $('#dev').click(function(){
-        $('#dev').toggle()
-        $('.dev').toggle()
-    })
+        $('#dev').toggle();
+        $('.dev').toggle();
+    });
     $('.dev').click(function(){
-        $('#dev').toggle()
-        $('.dev').toggle()
-    })
+        $('#dev').toggle();
+        $('.dev').toggle();
+    });
 
 
     $('#pro').click(function(){
-        $('#pro').toggle()
-        $('.pro').toggle()
-    })
+        $('#pro').toggle();
+        $('.pro').toggle();
+    });
     $('.pro').click(function(){
-        $('#pro').toggle()
-        $('.pro').toggle()
+        $('#pro').toggle();
+        $('.pro').toggle();
+    });
+
+
+    $(document).submit(function(event){
+        event.preventDefault();
+        var myName=$('#name').val();
+        var myEmail=$('#email').val();
+        alert("Hello " + myName + ", we have received your message, thank you for reaching out to us.")
     })
 
 });
